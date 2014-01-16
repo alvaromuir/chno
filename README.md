c6557c550c08ac90e91c4b43b43e705f7d3e730c
Alvaro Muir <alvaromuir@gmail.com>
2014-01-16 18:10:05 -0500

    Inital commit - Setup gem dependencies and run rspec generator

---
+++ README.md
@@ -0,0 +1,8 @@
+## CHNO
+
+#### The tool box for hacka-a-thons
+=====================================
+
+CHNO is an administrative tool for hack-a-thons.
+
+Alvaro Muir, @alvaromuir
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chno::Application.config.secret_key_base = '024513faefca3a045c3ac40a315020066cf8ebe7b937f27f9cdceac52275d65a1e2c2b9962e5e5981215f1442c459e2a321a1c0a25277daacc9846ba45f8da49'
