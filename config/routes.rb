b71b235f41dfbfc8b4b6011d9a40f0993f282f91
Alvaro Muir <alvaromuir@gmail.com>
2014-01-17 15:32:46 -0500

    Revert "User model with password"
    This reverts commit b37967cbcee20abfaa70efb19edc8dd8c72a12db.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Chno::Application.routes.draw do
-  get "users/new"
-  get "users/create"
-  get "users/show"
   root 'events#index'
 
   resources :events do
     resources :projects
   end
-
-  resources :users
 end
