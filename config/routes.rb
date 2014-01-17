b37967cbcee20abfaa70efb19edc8dd8c72a12db
Alvaro Muir <alvaromuir@gmail.com>
2014-01-17 15:31:48 -0500

    User model with password

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Chno::Application.routes.draw do
+  get "users/new"
+  get "users/create"
+  get "users/show"
   root 'events#index'
 
   resources :events do
     resources :projects
   end
+
+  resources :users
 end
